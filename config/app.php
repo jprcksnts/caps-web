e9b9c72f584f23be29251721a65745de18d18f38
Rick Santos <rick@code-vertical.com>
2020-06-07 16:33:32 +0800

    updated app and database configs for dev server deployment

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:EvEG7rkGrsHmMCto/QTQTGiE53CmCygvR+9tqQ2JJSw='),
 
     'cipher' => 'AES-256-CBC',
 
