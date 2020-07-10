1f62405d2a1fd382db7a3b3030ee39ec242da3e2
Rick Santos <rick@code-vertical.com>
2020-07-11 06:31:10 +0800

    updated configs for public_html root directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.config.publicPath='public_html';
+mix.js('resources/js/app.js', 'public_html/js')
+    .sass('resources/sass/app.scss', 'public_html/css');
