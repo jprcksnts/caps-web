ad36fde7dec628bd5b2c70f84d336b19fad22eb6
Rick Santos <snts.jprck@gmail.com>
2020-08-07 15:42:45 +0800

    updated welcome page, disabling input steps

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.landing.app')
 
+@section('styles')
+    <style>
+        /* DISABLE ARROWS ON INPUT TYPE NUMBER */
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        /* Firefox */
+        input[type=number] {
+            -moz-appearance: textfield;
+        }
+    </style>
+@endsection
+
 @section('content')
     <div class="main-content">
         <div class="header bg-gradient-header py-7 py-lg-7 pt-lg-6 text-center">
@@ -21,8 +38,8 @@
                                 @csrf
                                 <input type="email" class="form-control mb-2" id="email" name="email"
                                        placeholder="juandelacruz@example.com" required>
-                                <input type="number" class="form-control" id="contact_number" name="mobile"
-                                       placeholder="09260000000" required>
+                                <input type="number" class="form-control input-number" id="contact_number" name="mobile"
+                                       placeholder="09260000000"  required>
                                 <button class="btn btn-default btn-block mt-3" type="submit">Subscribe</button>
                             </form>
                         </div>
@@ -35,13 +52,23 @@
 
 @section('scripts')
     <script>
-        function phonenumber(inputtxt) {
-            var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-            if (inputtxt.value.match(phoneno)) {
-                return true;
-            } else {
-                return false;
-            }
+        // disable input number steps via up/down arrow keys
+        var inputElements = document.getElementsByClassName('input-number');
+        for (var i = 0; i < inputElements.length; i++) {
+            inputElements[i].addEventListener('keydown', function (e) {
+                if (e.which === 38 || e.which === 40) {
+                    e.preventDefault();
+                }
+            });
         }
+
+        // function phonenumber(inputtxt) {
+        //     var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+        //     if (inputtxt.value.match(phoneno)) {
+        //         return true;
+        //     } else {
+        //         return false;
+        //     }
+        // }
     </script>
 @endsection
